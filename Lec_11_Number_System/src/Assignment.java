3237adc677f13bef3a254b917f1862285649382d
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2023-03-02 23:54:55 +0530

    Added assignment codes ques 1 to 4 for Lec 11 number system

--- Lec_11_Number_System/src/Assignment.java
+++ Lec_11_Number_System/src/Assignment.java
@@ -2,5 +2,95 @@ import java.util.Scanner;
 public class Assignment {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        /*
+            First question
+            Q1 - Convert a decimal number into binary
+            Input1:
+            56
+            Output1:
+            111000
+            Input2:
+            163
+            Output2:
+            10100011
+         */
+//        int decNum = sc.nextInt();
+//        System.out.println(decimalToBinary(decNum));
+
+        /*
+            Second question
+            Q2 - Convert a binary number to decimal
+            Input1:
+            10100011
+            Output1:
+            163
+            Input2:
+            100011
+            Output2:
+            35
+         */
+
+//        int binNum = sc.nextInt();
+//        System.out.println(binaryToDecimal(binNum));
+
+        /*
+            Third question
+            Q3 - Add two numbers given in different base
+            Input1: 1st number is decimal, 2nd number is binary
+            43
+            10100011
+            Output1: In decimal number
+            206
+            Input2:
+            35
+            111000
+            Output2:
+            91
+         */
+
+//        int firstNo = sc.nextInt(), secondNo = sc.nextInt();
+//        int decSecondNo = binaryToDecimal(secondNo);
+//        System.out.println(firstNo + decSecondNo);
+
+        /*
+            Fourth question
+            Q4 - Multiply two numbers given in different base
+            Input1: 1st number is decimal, 2nd number is binary
+            43
+            10100011
+            Output1: In decimal number
+            7009
+            Input2:
+            56
+            100011
+            Output2:
+            1960
+         */
+
+        int mulFirstNo = sc.nextInt(), mulSecondNo = sc.nextInt();
+        int decMulSecondNo = binaryToDecimal(mulSecondNo);
+        System.out.println(mulFirstNo * decMulSecondNo);
+
+
+    }
+
+    static int decimalToBinary(int num){
+        int mul = 1, sum = 0;
+        while(num>0){
+            sum+=(num%2)*mul;
+            mul*=10;
+            num/=2;
+        }
+        return sum;
+    }
+
+    static int binaryToDecimal(int num){
+        int mul = 1, sum = 0;
+        while(num>0){
+            sum+=(num%10)*mul;
+            mul*=2;
+            num/=10;
+        }
+        return sum;
     }
 }
