6a1f95a909e8c5687912dd7b5025cb77a6470b84
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2023-03-03 23:03:18 +0530

    Added lecture codes for lec 12

---
+++ Lec_12_Bitwise_operators/src/Lecture.java
@@ -0,0 +1,11 @@
+public class Lecture {
+    public static void main(String[] args){
+        int a = 9, b = 10;
+        System.out.println(a&b); // 8
+        System.out.println(a|b); // 11
+        System.out.println(a^b); // 3
+        System.out.println(~a); // -10
+        System.out.println(a<<1); // 9*2 = 18
+        System.out.println(a>>1); // 9/2 = 4
+    }
+}
